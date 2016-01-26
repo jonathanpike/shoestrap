916c128813717ff0de7fbc5c86e5a33adb3f440f
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-26 06:22:37 -0500

    Fix error in rails engine file

--- lib/shoestrap-rails/engine.rb
+++ lib/shoestrap-rails/engine.rb
@@ -1,8 +1,8 @@
 module Shoestrap
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'bootstrap-sass.assets.precompile' do |app|
-        app.config.assets.paths << root.join('assets', stylesheets).to_s
+      initializer 'shoestrap-rails.assets.precompile' do |app|
+        app.config.assets.paths << root.join('assets', 'stylesheets').to_s
       end
     end
   end
