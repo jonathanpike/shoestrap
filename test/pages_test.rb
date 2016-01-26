5c7a8ef08bbae0540834f8c2aa85473059701b30
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2016-01-26 06:21:49 -0500

    Add tests from bootstrap

---
+++ test/pages_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper_rails'
+
+class PagesTest < ActionDispatch::IntegrationTest
+  include ::DummyRailsIntegration
+  include Capybara::DSL
+
+  def test_visit_root
+    visit root_path
+    # ^ will raise on JS errors
+
+    assert_equal 200, page.status_code
+
+    screenshot!
+  end
+end
